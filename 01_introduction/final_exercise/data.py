f0d2af80ab5c430384a8d0fa3b9744a3046af8b2
adamzenith <49098682+adamzenith@users.noreply.github.com>
2021-06-06 23:18:09 +0200

    asd

--- 01_introduction/final_exercise/data.py
+++ 01_introduction/final_exercise/data.py
@@ -2,6 +2,20 @@ import torch
 
 def mnist():
     # exchange with the real mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
-    return train, test
+
+
+    import torch
+    from torchvision import datasets, transforms
+  
+
+    # Define a transform to normalize the data
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5,), (0.5,))])
+    # Download and load the training data
+    trainset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+    # Download and load the test data
+    testset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=False, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+    return trainloader, testloader
